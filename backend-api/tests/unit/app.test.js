932fa42ee2d2ad15c335bf830643a408fa208c9c
darenc9 <devoncen@gmail.com>
2024-05-15 22:32:57 -0400

    jest unit tests

---
+++ backend-api/tests/unit/app.test.js
@@ -0,0 +1,25 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+const app = require('../../src/app');
+
+// Tests accessing a nonexistent route
+describe('404 Handler', () => {
+    test('should return a 404 status and error message', async () => {
+      const response = await request(app).get('/nonexistent-route');
+  
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({
+        message: "Resource not found",
+      });
+    });
+  });
+
+// Tests Health Check
+describe('Health Check', () => {
+    test('Health check returns healthy', async () => {
+        const response = await request(app).get('/');
+
+        expect(response.status).toBe(200);
+    });
+});
