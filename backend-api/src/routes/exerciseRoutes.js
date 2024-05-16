aa36eb40ad9ac36f8638a98dbd7412c63eafebd0
darenc9 <devoncen@gmail.com>
2024-05-15 20:30:56 -0400

    Exercise Data, exercise route

---
+++ backend-api/src/routes/exerciseRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const exercisesData = require('../data/exercises.json');
+
+
+router.get('/exercises', (req, res) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.status(200).json(exercisesData);
+});
+
+
+module.exports = router;
