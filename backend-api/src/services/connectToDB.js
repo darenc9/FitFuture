6b6913fdbcb48b23b52ead9664c1c24070f0f103
andrew.sequeira <asequeira@myseneca.ca>
2024-05-17 20:43:31 -0400

    require dot env added

--- backend-api/src/services/connectToDB.js
+++ backend-api/src/services/connectToDB.js
@@ -1,10 +1,17 @@
 // connection to database uses mongoose
+
+//required to get env variables when importing this file into another
+//for some reason env variables werent loaded without it
+require('dotenv').config();
 const mongoose = require('mongoose');
+
 
 const connection = {};    // to store our connection for checking if already connected
 
 // get our connection string
 let mongoDBConnectionString = process.env.MONGO_URL;
+
+console.log(mongoDBConnectionString);
 
 module.exports.connectToDb = function () {
   return new Promise((resolve, reject) => {
