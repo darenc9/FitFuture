6c62f2b575b1ef27b5e12eaeec732d714fb65158
andrew.sequeira <asequeira@myseneca.ca>
2024-05-18 20:45:56 -0400

    get all workouts route function implemented

--- backend-api/src/routes/workoutRoutes.js
+++ backend-api/src/routes/workoutRoutes.js
@@ -1,1 +1,16 @@
 // src/routes/workoutRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const workoutService = require('../services/workout-service');
+
+//Get a list of all preset workouts
+module.exports.getAll = async (req, res) => {
+    workoutService.getAllWorkouts()
+    .then(data => {
+      res.status(200).json(data);
+    }).catch(msg => {
+      res.status(404).json({error: msg});
+    })
+  };
+
