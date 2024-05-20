6cf79b606cdb9d8d099ba611d2e2d95376c2766a
andrew.sequeira <asequeira@myseneca.ca>
2024-05-19 21:35:29 -0400

    crud operations for workout exercise created

---
+++ backend-api/src/services/workoutExercise-service.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const WorkoutExercise = require('../schemas/WorkoutExercise');
+const { connectToDb } = require('./connectToDB');
+
+let WorkoutExerciseModel;
+
+// Ensure the database connection is established and the model is set up
+const ensureConnection = async () => {
+    if (!WorkoutExerciseModel) {
+        //use imported connect to db function to connect to mongo
+      await connectToDb();
+      WorkoutExerciseModel = mongoose.model('WorkoutExercise', WorkoutExercise.schema);
+    }
+};
+
+//get all workout exercises for a specific workout
+module.exports.getByWorkoutId = async function (workoutId) {
+    await ensureConnection();
+    return new Promise((resolve, reject) => {
+        WorkoutExerciseModel.find({ workoutId: workoutId }).exec()
+        .then((workoutExercise) => {
+          if (workoutExercise.length > 0) {
+            resolve(workoutExercise);
+          } else {
+            reject(`No workoutExercise records found where id is ${id}`);
+          }
+        })
+        .catch((err) => reject('Error retrieving workoutExercise records: ' + err.message));
+    });
+  };
+
+//get a workout exercise by id (not sure if will be needed)
+  module.exports.getWorkoutExerciseById = async function (workoutExerciseId) {
+    await ensureConnection();
+    return new Promise((resolve, reject) => {
+      WorkoutModel.findById(workoutExerciseId).exec()
+        .then((workoutExercise) => {
+          if (workoutExercise) {
+            resolve(workoutExercise);
+          } else {
+            reject('workout exercise record not found');
+          }
+        })
+        .catch((err) => reject('Error retrieving workout record: ' + err.message));
+    });
+  };
+
+// Create a new workout exercise
+module.exports.createWorkoutExercise = async function (data) {
+    await ensureConnection();
+    return new Promise((resolve, reject) => {
+      const newWorkoutExercise = new WorkoutExerciseModel(data);
+      newWorkoutExercise.save()
+        .then((workoutExercise) => resolve(workoutExercise))
+        .catch((err) => reject('Error creating workout exercise: ' + err.message));
+    });
+  };
+
+  // Update a workout exercise by id
+module.exports.updateWorkoutExercise = async function (workoutExerciseId, data) {
+    await ensureConnection();
+    return new Promise((resolve, reject) => {
+        //new: true ensures new (updated document is returned rather than original)
+      WorkoutExerciseModel.findByIdAndUpdate(workoutExerciseId, data, { new: true }).exec()
+        .then((workoutExercise) => {
+          if (workoutExercise) {
+            resolve(workoutExercise);
+          } else {
+            reject('workout exercise record not found');
+          }
+        })
+        .catch((err) => reject('Error updating workout exercise: ' + err.message));
+    });
+  };
+
+  // Delete a workout exercise by id (may not be needed)
+module.exports.deleteWorkoutExercise = async function (workoutExerciseId) {
+    await ensureConnection();
+    return new Promise((resolve, reject) => {
+      WorkoutExerciseModel.findByIdAndDelete(workoutExerciseId).exec()
+        .then((workoutExercise) => {
+          if (workoutExercise) {
+            resolve('Workout exercise record deleted successfully');
+          } else {
+            reject('workout exercise record not found');
+          }
+        })
+        .catch((err) => reject('Error deleting workout exercise: ' + err.message));
+    });
+  };
