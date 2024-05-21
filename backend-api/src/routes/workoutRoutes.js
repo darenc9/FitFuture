810de15bb45f4abf2dfc0514bfcfcbad26bbdcff
andrew.sequeira <asequeira@myseneca.ca>
2024-05-21 12:55:33 -0400

    get by id function added

--- backend-api/src/routes/workoutRoutes.js
+++ backend-api/src/routes/workoutRoutes.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const workoutService = require('../services/workout-service');
+const workoutExerciseService = require('../services/workoutExercise-service');
 
 //Get a list of all preset workouts
 module.exports.getAll = async (req, res) => {
@@ -13,4 +14,16 @@ module.exports.getAll = async (req, res) => {
       res.status(404).json({error: msg});
     })
   };
+
+  //Get a list of all workout exercises for specific workout (ie get workout by id)
+module.exports.getById = async (req, res) => {
+  const id = req.params.id;
+  workoutExerciseService.getWorkoutExerciseByWorkoutId(id)
+  .then(data => {
+    res.status(200).json(data);
+  }).catch(msg => {
+    res.status(404).json({error: msg});
+  })
+};
+
 
