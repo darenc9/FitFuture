a234431067166113ecb8bd2739197877ef4a49e1
darenc9 <devoncen@gmail.com>
2024-05-21 20:23:01 -0400

    healthcheck frontpage

--- frontend-ui/src/app/page.js
+++ frontend-ui/src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import HealthCheck from "@/components/HealthCheck";
 
 export default function Home() {
   return (
     <main>
+      <HealthCheck />
 
     </main>
   );
