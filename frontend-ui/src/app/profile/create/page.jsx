19713e8c96fabeed44014664cd2c81990d695aa2
mfunkel <maraperkons@gmail.com>
2024-05-23 18:18:07 -0400

    adds create profile page

---
+++ frontend-ui/src/app/profile/create/page.jsx
@@ -0,0 +1,13 @@
+// page to create a new profile
+
+import AddEdit from "@/components/profile/AddEdit";
+
+const AddProfilePage = () => {
+  return (
+    <div>
+      <AddEdit />
+    </div>
+  )
+};
+
+export default AddProfilePage;
