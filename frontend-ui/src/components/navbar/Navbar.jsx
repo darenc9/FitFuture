1a5c1b2d20ec495230e2489ceac51296234de9c5
mfunkel <maraperkons@gmail.com>
2024-05-26 12:50:51 -0400

    redo navbar with icons

--- frontend-ui/src/components/navbar/Navbar.jsx
+++ frontend-ui/src/components/navbar/Navbar.jsx
@@ -3,27 +3,47 @@
 "use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { UserIcon, WrenchScrewdriverIcon, MagnifyingGlassIcon, ChartBarIcon, HomeIcon } from '@heroicons/react/24/solid';
 
 
 const Navbar = () => {
-  
-  const links = [
-    { title: "Home", path: "/home" },
-    { title: "Profile", path: "/profile" },
-    { title: "Browse", path: "/browse" },
-    { title: "Build", path: "/build" },
-    { title: "Progress", path: "/progress" }
-  ];
+
   const pathName = usePathname();
 
   return (
     <div className="flex justify-between container mx-auto p-4">
-      <div className="font-bold text-lg content-center">FitFuture</div>
       <div>
-        <div className="flex space-x-4">
-          {links.map(link => (
-            <Link href={link.path} key={link.path} className={`rounded p-1 ${pathName == link.path ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}>{link.title}</Link>
-          ))}
+        <Link href={'/home'} key={'/home'} className={`flex flex-col items-center ${pathName == '/home' ? 'text-blue-500' : 'text-auto'}`}>
+          <HomeIcon className="size-6 "/>
+          <span>Home</span>
+        </Link>
+      </div>
+      <div>
+        <div className="flex space-x-2">
+          <div>
+            <Link href={'/profile'} key={'/profile'} className={`flex flex-col items-center ${pathName == '/profile' ? 'text-blue-500' : 'text-auto'}`}>
+              <UserIcon className="size-6 "/>
+              <span>Profile</span>
+            </Link>
+          </div>
+          <div>
+            <Link href={'/browse'} key={'/browse'} className={`flex flex-col items-center ${pathName == '/browse' ? 'text-blue-500' : 'text-auto'}`}>
+              <MagnifyingGlassIcon className="size-6 "/>
+              <span>Browse</span>
+            </Link>
+          </div>
+          <div>
+            <Link href={'/build'} key={'/build'} className={`flex flex-col items-center ${pathName == '/build' ? 'text-blue-500' : 'text-auto'}`}>
+              <WrenchScrewdriverIcon className="size-6 "/>
+              <span>Build</span>
+            </Link>
+          </div>
+          <div>
+            <Link href={'/progress'} key={'/progress'} className={`flex flex-col items-center ${pathName == '/progress' ? 'text-blue-500' : 'text-auto'}`}>
+              <ChartBarIcon className="size-6 "/>
+              <span>Progress</span>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
