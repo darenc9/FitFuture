b9ad65530adf6733ecdfb70fd643ac4714d0c022
andrew.sequeira <asequeira@myseneca.ca>
2024-06-13 18:51:36 -0400

    updated naming convention for better clarity about update vs save

--- frontend-ui/src/components/edit/WorkoutEdit.jsx
+++ frontend-ui/src/components/edit/WorkoutEdit.jsx
@@ -61,7 +61,7 @@ const WorkoutEdit = () => {
     }
   }, [workout]);
 
-  const handleSaveWorkout = async () => {
+  const handleUpdateWorkout = async () => {
     if (!workout.name) {
       setError('Please enter a workout name.');
       return;
@@ -126,10 +126,10 @@ const WorkoutEdit = () => {
         </div>
         <div className="mt-8 mb-4">
           <button
-            onClick={handleSaveWorkout}
+            onClick={handleUpdateWorkout}
             className="w-full px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
           >
-            Save Workout
+            Update Workout
           </button>
         </div>
       </div>
