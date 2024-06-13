670852e215d9dbf2e70af7df24ed4e452972923e
andrew.sequeira <asequeira@myseneca.ca>
2024-06-13 00:54:42 -0400

    add ability to edit public status and name + save button

--- frontend-ui/src/components/edit/WorkoutEdit.jsx
+++ frontend-ui/src/components/edit/WorkoutEdit.jsx
@@ -8,80 +8,113 @@ import { editWorkoutAtom, setEditExerciseAtoms } from '../../../utility/editExer
 import { useAtomValue } from 'jotai';
 
 const WorkoutEdit = () => {
-    const { id } = useParams();
-    const [currentUser] = useAtom(profileIdAtom);
-    const [workout, setWorkout] = useAtom(editWorkoutAtom);
-    const router = useRouter();
-    //useAtomValue is used to read current value of editWorkoutAtom without causing re-renders
-    const currentWorkout = useAtomValue(editWorkoutAtom);
+  const { id } = useParams();
+  const [currentUser] = useAtom(profileIdAtom);
+  const [workout, setWorkout] = useAtom(editWorkoutAtom);
+  const router = useRouter();
+  const currentWorkout = useAtomValue(editWorkoutAtom);
+  const [isPublic, setIsPublic] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                //only fetch if currentWorkout isnt already populated
-                if (!currentWorkout.workoutId) {
-                    const res = await fetch(`http://localhost:8080/workout/${id}`);
-                    if (!res.ok) {
-                        throw new Error(`Failed to fetch workout with id: ${id}`);
-                    }
-                    const exerciseData = await res.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Only fetch if currentWorkout isn't already populated
+        if (!currentWorkout.workoutId) {
+          console.log("fetching data");
+          const res = await fetch(`http://localhost:8080/workout/${id}`);
+          if (!res.ok) {
+            throw new Error(`Failed to fetch workout with id: ${id}`);
+          }
+          const exerciseData = await res.json();
 
-                    const res2 = await fetch(`http://localhost:8080/workouts/${id}`);
-                    if (!res2.ok) {
-                        throw new Error(`Failed to fetch workout with id: ${id}`);
-                    }
-                    const workoutData = await res2.json();
+          const res2 = await fetch(`http://localhost:8080/workouts/${id}`);
+          if (!res2.ok) {
+            throw new Error(`Failed to fetch workout with id: ${id}`);
+          }
+          const workoutData = await res2.json();
 
-                    // Populate editWorkoutAtom
-                    setWorkout({
-                        workoutId: workoutData.workoutId,
-                        name: workoutData.name,
-                        exerciseIds: exerciseData.map(ex => ex.exerciseId),
-                        public: workoutData.public,
-                    });
+          // Populate editWorkoutAtom
+          setWorkout({
+            workoutId: workoutData.workoutId,
+            name: workoutData.name,
+            exerciseIds: exerciseData.map(ex => ex.exerciseId),
+            public: workoutData.public,
+          });
 
-                    // Populate exercise atoms
-                    setEditExerciseAtoms(exerciseData);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
+          // Populate exercise atoms
+          setEditExerciseAtoms(exerciseData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchData();
-        
-    }, [id, setWorkout, currentWorkout]);
+    fetchData();
+  }, [id, setWorkout, currentWorkout]);
 
-    return (
-        <div className="container mx-auto px-4">
-            <h1 className="text-2xl font-bold text-center mt-8">Edit Workout</h1>
-            <div className="mt-8 space-y-4">
-                <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-700 mb-2">Workout Name:</label>
-                    <input
-                        type="text"
-                        value={workout.name}
-                        readOnly
-                        className="block w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                    />
-                </div>
-                <div className="flex items-center mb-4">
-                    <label className="block text-sm font-medium text-gray-700 mr-2">Public:</label>
-                    <input
-                        type="checkbox"
-                        checked={workout.public}
-                        readOnly
-                        className="form-checkbox h-5 w-5 text-blue-600"
-                    />
-                </div>
-                <div className="space-y-4">
-                    {workout.exerciseIds.map((exerciseId) => (
-                        <ExercisePanel key={exerciseId} id={exerciseId} workoutId={workout.workoutId} />
-                    ))}
-                </div>
-            </div>
+  useEffect(() => {
+    // Update isPublic whenever workout is updated
+    if (workout.public !== undefined) {
+      setIsPublic(workout.public);
+    }
+  }, [workout]);
+
+  const handleSaveWorkout = async () => {
+    if (!workout.name) {
+      setError('Please enter a workout name.');
+      return;
+    }
+
+    if (workout.exerciseIds.length === 0) {
+      setError('Please add at least one exercise to the workout.');
+      return;
+    }
+
+    router.push('/workouts/edit/updateWorkout?workoutId=' + workout.workoutId);
+  };
+
+  const handleCheckboxChange = (e) => {
+    setWorkout({ ...workout, public: e.target.checked });
+  };
+
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold text-center mt-5">Edit Workout</h1>
+      <div className="mt-6 space-y-4">
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-2">Workout Name:</label>
+          <input
+            type="text"
+            value={workout.name}
+            onChange={(e) => setWorkout({ ...workout, name: e.target.value })}
+            className="block w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
         </div>
-    );
+        <div className="flex items-center mb-4">
+          <label className="block text-sm font-medium text-gray-700 mr-2">Public:</label>
+          <input
+            type="checkbox"
+            checked={isPublic}
+            onChange={handleCheckboxChange}
+            className="form-checkbox h-5 w-5 text-blue-600"
+          />
+        </div>
+        <div className="space-y-4">
+          {workout.exerciseIds.map((exerciseId) => (
+            <ExercisePanel key={exerciseId} id={exerciseId} workoutId={workout.workoutId} />
+          ))}
+        </div>
+        <div className="mt-8 mb-4">
+          <button
+            onClick={handleSaveWorkout}
+            className="w-full px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+          >
+            Save Workout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkoutEdit;
