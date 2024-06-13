3738068213544967cfcd157e40b68a9f8742162a
andrew.sequeira <asequeira@myseneca.ca>
2024-06-12 20:59:27 -0400

    pass workoutId so next page can reroute back after delete

--- frontend-ui/src/components/edit/ExercisePanel.jsx
+++ frontend-ui/src/components/edit/ExercisePanel.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useAtom } from 'jotai';
 import { getEditExerciseAtom } from '../../../utility/editExerciseAtom';
+import { TrashIcon } from '@heroicons/react/24/solid';
+import { useRouter } from 'next/navigation';
 
-const ExercisePanel = ({ id }) => {
+const ExercisePanel = ({ id, workoutId }) => {
     const exerciseAtom = getEditExerciseAtom(id);
     const [exercise] = useAtom(exerciseAtom);
+    const router = useRouter();
 
     const getImage = (id) => {
         return `/exercises/${id}/0.jpg`;
     };
+
+    const handleDelete = () => {
+        router.push(`/workouts/edit/deleteExercise?id=${id}&wId=${workoutId}`);
+      };
 
     return (
         <div className="flex items-center p-4 border rounded-md shadow-md bg-white mb-4">
@@ -21,6 +28,9 @@ const ExercisePanel = ({ id }) => {
                 <h3 className="text-lg font-bold">{exercise.name}</h3>
                 <p className="text-sm">{exercise.sets}x - {exercise.reps} reps</p>
             </div>
+            <button onClick={handleDelete} className="text-red-500 hover:text-red-700">
+            <TrashIcon className="w-5 h-5" />
+        </button>
         </div>
     );
 };
