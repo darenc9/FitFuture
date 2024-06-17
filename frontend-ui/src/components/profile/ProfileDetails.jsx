5631abb00a0d17c2d238dfbe14f57d15934216c5
mfunkel <maraperkons@gmail.com>
2024-06-17 15:21:45 -0400

    update form and detail display to use username and DoB instead of age

--- frontend-ui/src/components/profile/ProfileDetails.jsx
+++ frontend-ui/src/components/profile/ProfileDetails.jsx
@@ -1,12 +1,28 @@
+
+// function to calculate the user's age based on their profile's dob attribute
+function calculateAge(dob) {
+  var birthDate = new Date(dob);
+  var currentDate = new Date();
+
+  var numYears = (currentDate.getFullYear() - birthDate.getFullYear());
+
+  if (currentDate.getMonth() < birthDate.getMonth() ||
+      currentDate.getMonth() == birthDate.getMonth() && currentDate.getDate() < birthDate.getDate()) {
+    numYears--;
+  }
+
+  return numYears;
+}
+
 // component to display Profile details
 export default function ProfileDetails( {profile} ) {
   return (
     <div className="mx-auto p-4">
-      <h1 className="text-xl font-bold text-center">Your Profile Information</h1>
+      <h1 className="text-xl font-bold text-center">{profile.userId}'s Profile</h1>
       <div className="flex flex-col space-y-2 mt-5">
-        <p><span className="font-semibold">Age: </span>{profile.age}</p>
+        <p><span className="font-semibold">Age: </span>{calculateAge(profile.dob)}</p>
         <p><span className="font-semibold">Height: </span>{profile.height} cm</p>
-        <p><span className="font-semibold">Weight: </span>{profile.weight} lbs</p>
+        <p><span className="font-semibold">Weight: </span>{profile.weight} kgs</p>
         <p><span className="font-semibold">Sex: </span>{profile.sex}</p>
         <p><span className="font-semibold">Fitness Level: </span>{profile.fitnessLevel}</p>
       </div>
