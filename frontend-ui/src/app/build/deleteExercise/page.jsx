d2c7b399d5752982ea5ad596b8baa244f3ee9d98
andrew.sequeira <asequeira@myseneca.ca>
2024-06-17 23:33:20 -0400

    update to wrap in suspense boundary for vercel fix

--- frontend-ui/src/app/build/deleteExercise/page.jsx
+++ frontend-ui/src/app/build/deleteExercise/page.jsx
@@ -1,27 +1,12 @@
-"use client"
-import { useEffect } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation'; // Import from 'next/navigation'
-import { useAtom } from 'jotai';
-import { workoutAtom, removeExerciseAtom } from '../../../../utility/exerciseAtom'; // Import removeExerciseAtom
+import React, { Suspense } from 'react';
+import DeleteExercisePage from '../../../components/build/DeleteExercisePage'; // Adjust the import path
 
-const DeleteExercisePage = () => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id'); // Access the query parameter
-  const router = useRouter();
-  const [workout, setWorkout] = useAtom(workoutAtom);
-
-  useEffect(() => {
-    if (id) {
-      setWorkout((prevWorkout) => ({
-        ...prevWorkout,
-        exerciseIds: prevWorkout.exerciseIds.filter(exerciseId => exerciseId !== id),
-      }));
-      removeExerciseAtom(id); // Remove the exercise atom
-      router.push('/build'); // Navigate back to the previous page
-    }
-  }, [id, setWorkout, router]);
-
-  return; //dont return anything for display
+const DeleteExercisePageWrapper = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DeleteExercisePage />
+    </Suspense>
+  );
 };
 
-export default DeleteExercisePage;
+export default DeleteExercisePageWrapper;
