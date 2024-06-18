a8763c00e784e67046fcb112e2dd69f36fad2259
mfunkel <maraperkons@gmail.com>
2024-06-18 13:27:04 -0400

    add initial history components

---
+++ frontend-ui/src/components/history/HistoryFilter.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const HistoryFilter = ({ catFilter, startDate, endDate, order, handleFilterChange, handleStartDateChange, handleEndDateChange, handleOrderChange }) => {
+    return (
+    <div className='mx-auto p-2'>
+      <div className="flex items-center justify-evenly">
+        <div className='space-x-2'>
+          <input type="date" 
+              value={startDate}
+              onChange={handleStartDateChange}
+              className='rounded ring m-1'/>
+          <span>to</span>
+          <input type="date"
+              value={endDate}
+              onChange={handleEndDateChange}
+              className='rounded ring'/>
+        </div>
+      </div>
+      <div className='flex justify-end gap-2 py-2'>
+        <label>Category: </label>
+        <select value={catFilter} onChange={handleFilterChange} className="right-0 px-2 py-1 rounded border text-sm">
+            <option value="all">All</option>
+            <option value="cardio">Cardio</option>
+            <option value="strength">Strength</option>
+            <option value="flexibility">Flexibility</option>
+            <option value="yoga">Yoga</option>
+            {/* Add more options as needed */}
+        </select>
+        <label>Sort: </label>
+        <select value={order} onChange={handleOrderChange} className="right-0 px-2 py-1 rounded border text-sm">
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+        </select>
+      </div>
+    </div>
+    );
+};
+
+export default HistoryFilter;
---
+++ frontend-ui/src/components/history/HistoryList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const HistoryList = ({ histories, handlePanelClick }) => {
+  
+  return (
+    <div className="mt-4 space-y-4">
+      {(Array.isArray(histories) && !histories.length) && 
+        <p className='text-center text-sm'>No history results! Try changing your filters.</p>
+      }
+      {histories.map(history => (
+        <div
+          key={history._id}
+          className="flex items-center p-4 border rounded-lg shadow-md cursor-pointer hover:bg-gray-100"
+          onClick={() => handlePanelClick(history)}
+        >
+
+          <div className="ml-4">
+            <h3 className="text-lg font-semibold">{history.exerciseName}</h3>
+            <p className="text-gray-600">{new Date(history.date).toISOString().split('T')[0]}</p>
+          </div>
+          <div className="ml-auto">
+            {history.duration && <p className="text-sm font-semibold">{Math.floor(history.duration / 60)} min {Math.floor(history.duration % 60)} sec</p> || null }
+            {history.reps && <p className="text-sm font-semibold">{history.reps} x {history.sets} sets</p> || null }
+            {history.weight && <p className="text-sm font-semibold">{history.weight} lbs</p> || null }
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HistoryList;
