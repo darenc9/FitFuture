2e53c02d45705c0bea7ce7e05b6ca8080ebed8d9
darenc9 <devoncen@gmail.com>
2024-06-18 14:31:14 -0400

    exercises, authentication test, deployment bugs

--- frontend-ui/src/app/page.js
+++ frontend-ui/src/app/page.js
@@ -1,11 +1,23 @@
-import Image from "next/image";
+"use client";
 import HealthCheck from "@/components/HealthCheck";
+import React from 'react';
+import Navbar from "@/components/navbar/Navbar";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
-export default function Home() {
+function Home() {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
   return (
-    <main>
-      <HealthCheck />
-
-    </main>
+    <main className="flex flex-col items-center justify-center h-screen">
+    <h1 className="mb-4 text-2xl font-bold">Hello {user.username}</h1>
+    <button 
+      onClick={signOut} 
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition"
+    >
+      Sign out
+    </button>
+  </main>
   );
 }
+export default withAuthenticator(Home);
--- frontend-ui/src/components/exercises/ExerciseTable.jsx
+++ frontend-ui/src/components/exercises/ExerciseTable.jsx
@@ -1,6 +1,32 @@
+// src/components/exercises/ExerciseTable.jsx
 import React from "react";
+import Link from 'next/link';
+import { useAtom } from 'jotai';
+import { FaCheckSquare, FaRegSquare } from 'react-icons/fa';
+import { selectedExercisesAtom } from '@/atoms/exercisesPageAtoms';
 
-const ExerciseTable = ({ exercises }) => {
+const ExerciseTable = ({ exercises, selectedExercises, setSelectedExercises}) => {
+
+  const handleSelectExercise = (exercise) => {
+    setSelectedExercises((prevSelected) => {
+      if (prevSelected.includes(exercise)) {
+        return prevSelected.filter((e) => e !== exercise);
+      } else {
+        return [...prevSelected, exercise];
+      }
+    });
+  };
+
+  const isSelected = (exercise) => {
+    return selectedExercises.some(e => e.id === exercise.id)
+  };
+
+  const handleSendButtonClick = () => {
+    // Implement send button functionality here
+    console.log("Send button clicked");
+    // You can perform any action here, such as submitting selected exercises
+  };
+
   return (
     <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
       <thead className="bg-gray-800 text-white">
@@ -11,23 +37,34 @@ const ExerciseTable = ({ exercises }) => {
           <th className="w-1/5 text-left py-3 px-4 uppercase font-semibold text-sm hidden md:table-cell">Mechanic</th>
           <th className="w-1/5 text-left py-3 px-4 uppercase font-semibold text-sm hidden md:table-cell">Equipment</th>
           <th className="w-1/5 text-left py-3 px-4 uppercase font-semibold text-sm">Primary Muscle Group</th>
+          <th className="w-1/5 text-left py-3 px-4 uppercase font-semibold text-sm"></th>
         </tr>
       </thead>
       <tbody className="text-gray-700">
         {exercises.map((exercise) => (
-          <tr key={exercise.id}>
+          <tr key={exercise.id} className={`${isSelected(exercise) ? 'bg-blue-100' : ''}`} >
             <td className="w-1/5 text-left py-3 px-4">
-              <a href={`http://localhost:3000/exercises/${encodeURIComponent(exercise.id)}`}>
+              <Link href={`/exercises/${encodeURIComponent(exercise.id)}`}>
                 {exercise.name}
-              </a>
+              </Link>
             </td>
-            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell">{exercise.force}</td>
-            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell">{exercise.level}</td>
-            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell">{exercise.mechanic}</td>
-            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell">{exercise.equipment}</td>
-            <td className="w-1/5 text-left py-3 px-4">{exercise.primaryMuscles.join(', ')}</td>
+            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell" onClick={() => handleSelectExercise(exercise)}>{exercise.force}</td>
+            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell" onClick={() => handleSelectExercise(exercise)}>{exercise.level}</td>
+            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell" onClick={() => handleSelectExercise(exercise)}>{exercise.mechanic}</td>
+            <td className="w-1/5 text-left py-3 px-4 hidden md:table-cell" onClick={() => handleSelectExercise(exercise)}>{exercise.equipment}</td>
+            <td className="w-1/5 text-left py-3 px-4"  onClick={() => handleSelectExercise(exercise)}>{exercise.primaryMuscles.join(', ')}</td>
+            <td className="py-2 text-center cursor-pointer" onClick={() => handleSelectExercise(exercise)}>
+              {isSelected(exercise) ? <FaCheckSquare className="text-blue-500 icon" /> : <FaRegSquare className="text-gray-500 icon" />}
+            </td>
           </tr>
         ))}
+        <tr>
+          <td colSpan="7" className="text-right py-3 px-4">
+            <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={handleSendButtonClick}>
+              Confirm
+            </button>
+          </td>
+        </tr>
       </tbody>
     </table>
   );
