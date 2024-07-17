5fff261e8666a6174a17a877ae6c2a4fbf021947
mfunkel <maraperkons@gmail.com>
2024-07-17 12:09:34 -0400

    add favourite routines functionality

--- frontend-ui/src/components/favourites/FavouritesList.jsx
+++ frontend-ui/src/components/favourites/FavouritesList.jsx
@@ -3,6 +3,7 @@ import FavWorkoutCard from "./FavWorkoutCard";
 import { profileAtom } from "../../../store";
 import { GetToken } from "../AWS/GetToken";
 import { useRouter } from "next/navigation";
+import FavRoutineCard from "./FavRoutineCard";
 
 // component to display list of user's favourites
 export default function FavouritesList( {favWorkouts, favRoutines} ) {
@@ -46,13 +47,66 @@ export default function FavouritesList( {favWorkouts, favRoutines} ) {
     }
   };
 
+  const handleFavRoutineClicked = async (routine) => {
+    var favs = profile?.favourites;
+
+    // either add or remove from the profile atom first
+    if (profile?.favourites.routines.findIndex((rt) => rt._id == routine._id) !== -1) {
+      // remove the routine from favs
+      console.debug('remove routine from favs');
+      favs.routines = favs.routines.filter(rt => rt._id !== routine._id);
+    } else {
+      // add routine to favs
+      console.debug('add routine to favs');
+      favs.routines.push(routine);
+    }
+
+    // then save it to the database
+    try {
+      const authToken = await GetToken();
+      const res = await fetch(`${API_URL}/profile/favourites/${profile._id}`, {
+        method: "PUT",
+        headers: {
+          'Authorization': `Bearer ${authToken}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(favs),
+      });
+      if (!res.ok) {
+        throw new Error(`Failed to update profile favourites for id: ${profile._id}`);
+      }
+      const resData = await res.json();
+      return resData;
+    } catch (error) {
+      console.error('Error updating profile favourites: ', error);
+      return null;
+    }
+  };
+
   const handleWorkoutPanelClicked = (workout) => {
     router.push(`/workouts/${workout.workoutId}`);
   };
 
+  const handleRoutinePanelClicked = (routine) => {
+    router.push(`/routines/${routine.routineId}`);
+  };
+
   return (
     <div className="w-screen mx-auto p-4 flex flex-col gap-2">
-      {favWorkouts.map(workout => <FavWorkoutCard key={workout._id} workout={workout} handleFavWorkoutClicked={handleFavWorkoutClicked} handleWorkoutPanelClicked={handleWorkoutPanelClicked}/>)}
+      {favRoutines.map(routine =>
+        <FavRoutineCard key={routine._id}
+        routine={routine}
+        handleFavRoutineClicked={handleFavRoutineClicked}
+        handleRoutinePanelClicked={handleRoutinePanelClicked}
+        />
+      )}
+      {favWorkouts.map(workout => 
+        <FavWorkoutCard key={workout._id}
+        workout={workout}
+        handleFavWorkoutClicked={handleFavWorkoutClicked} 
+        handleWorkoutPanelClicked={handleWorkoutPanelClicked}
+        />
+      )}
     </div>
   );
 };
