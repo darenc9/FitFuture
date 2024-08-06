f61535e5c05b0223fd690c7626c1ccb0ea2fc240
mfunkel <maraperkons@gmail.com>
2024-08-06 14:23:36 -0400

    Adjusted workout completion to store history sets with numbers instead of strings

--- backend-api/src/routes/workoutRoutes.js
+++ backend-api/src/routes/workoutRoutes.js
@@ -218,6 +218,11 @@ module.exports.createHistory = async (req, res) => {
   console.log(data.exercises[0].sets);
 
   for (var i = 0; i < exercises.length; i++){
+    // convert the set's weight and reps values from string to number
+    for (const set of exercises[i].sets) {
+      set.weight = parseFloat(set.weight);
+      set.reps = parseInt(set.reps);
+    }
     // Create the History object
     const historyData = {
       historyId: new mongoose.Types.ObjectId(), // Generate a new ObjectId for historyId
